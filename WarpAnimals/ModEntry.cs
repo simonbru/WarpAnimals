224d16371bf2ba959bc218378520f9202b31e03e
Simon <simon@brulhart.me>
2018-05-02 11:54:48 +0200

    Update mod for Stardew Valley 1.3 beta

--- WarpAnimals/ModEntry.cs
+++ WarpAnimals/ModEntry.cs
@@ -1,10 +1,8 @@
 ﻿﻿using System;
 using System.Collections.Generic;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewModdingAPI.Utilities;
 using StardewValley;
 
 namespace WarpAnimals {
